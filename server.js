f83b5cf3592d2148284903e0c2008cc52b3a23fe
AllanEl <allanrnelias@gmail.com>
2022-12-11 02:43:42 -0500

    html header done

---
+++ server.js
@@ -0,0 +1,22 @@
+// import dependencies
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Sets up the routes
+app.use(require('./controllers/dish-routes'));
+
+//Starts the server to begin listening
+app.listen(PORT, () => {
+    console.log('server listening on: http://localhost:' + PORT)
+});
