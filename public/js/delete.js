d7eef0540448fbd71ffb60ee0661e22da0bb88f1
AllanEl <allanrnelias@gmail.com>
2023-01-06 01:42:12 -0500

    cant delete pt2

--- public/js/delete.js
+++ public/js/delete.js
@@ -9,7 +9,7 @@ const delButtonHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      location.reload();
     } else {
       alert('Failed to delete post');
     }
